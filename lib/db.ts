ac5170cf6518b7ce28137c5cc6da3c691aa703c0
Masoud Rezakhanlou <masoud.rk95@gmail.com>
2025-03-04 01:16:59 +0400

    global prisma update

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,20 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// export const db = globalThis.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
+export const db =
   globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
