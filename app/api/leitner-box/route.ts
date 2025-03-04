8cba1d82f3e93245419e61387bb8552fb1054ce5
Masoud Rezakhanlou <masoud.rk95@gmail.com>
2025-03-04 14:10:06 +0400

    fix build

--- app/api/leitner-box/route.ts
+++ app/api/leitner-box/route.ts
@@ -18,23 +18,23 @@ export async function GET() {
 // POST /api/notes
 export async function POST(request: Request) {
 
-    const session = await auth()
-    try {
-        const { content } = await request.json();
-        const newNote = await db.note.create({
-            data: {
-                userId: session?.user.id!,
-                content,
-                createdAt: new Date(),
-            },
-        });
-        return NextResponse.json(newNote, { status: 201 });
-    } catch (error) {
-        return NextResponse.json(
-            { error: 'Failed to create note' },
-            { status: 500 }
-        );
-    }
+    // const session = await auth()
+    // try {
+    //     const { content } = await request.json();
+    //     const newNote = await db.note.create({
+    //         data: {
+    //             userId: session?.user.id!,
+    //             content,
+    //             createdAt: new Date(),
+    //         },
+    //     });
+    //     return NextResponse.json(newNote, { status: 201 });
+    // } catch (error) {
+    //     return NextResponse.json(
+    //         { error: 'Failed to create note' },
+    //         { status: 500 }
+    //     );
+    // }
 }
 
 // PUT /api/notes/:id
@@ -42,20 +42,20 @@ export async function PUT(
     request: Request,
     { params }: { params: { id: string } }
 ) {
-    try {
-        const { id } = params;
-        const { content } = await request.json();
-        const updatedNote = await db.note.update({
-            where: { id },
-            data: { content },
-        });
-        return NextResponse.json(updatedNote);
-    } catch (error) {
-        return NextResponse.json(
-            { error: 'Failed to update note' },
-            { status: 500 }
-        );
-    }
+    // try {
+    //     const { id } = params;
+    //     const { content } = await request.json();
+    //     const updatedNote = await db.note.update({
+    //         where: { id },
+    //         data: { content },
+    //     });
+    //     return NextResponse.json(updatedNote);
+    // } catch (error) {
+    //     return NextResponse.json(
+    //         { error: 'Failed to update note' },
+    //         { status: 500 }
+    //     );
+    // }
 }
 
 // DELETE /api/notes/:id
@@ -63,16 +63,16 @@ export async function DELETE(
     request: Request,
     { params }: { params: { id: string } }
 ) {
-    try {
-        const { id } = params;
-        await db.note.delete({
-            where: { id },
-        });
-        return new NextResponse(null, { status: 204 });
-    } catch (error) {
-        return NextResponse.json(
-            { error: 'Failed to delete note' },
-            { status: 500 }
-        );
-    }
+    // try {
+    //     const { id } = params;
+    //     await db.note.delete({
+    //         where: { id },
+    //     });
+    //     return new NextResponse(null, { status: 204 });
+    // } catch (error) {
+    //     return NextResponse.json(
+    //         { error: 'Failed to delete note' },
+    //         { status: 500 }
+    //     );
+    // }
 }
